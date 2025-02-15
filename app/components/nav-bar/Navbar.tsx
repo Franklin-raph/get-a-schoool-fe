dac2a62005dca256e9d4959412aba973ba7005c8
franklin-raph <igboekwulusifranklin@gmail.com>
2025-02-15 23:00:54 +0100

    update

--- app/components/nav-bar/Navbar.tsx
+++ app/components/nav-bar/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../../public/images/Get-a-school.png'
 import Image from 'next/image'
+import { BiMenu } from 'react-icons/bi'
+import { IoCloseOutline } from 'react-icons/io5'
 
 export default function Navbar() {
+
+    const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
     const navLinks = [
         {
@@ -30,11 +36,11 @@ export default function Navbar() {
 
   return (
     <div className='flex items-center justify-between max-w-[1600px] mx-auto md:px-[4rem] px-[1rem] py-4'>
-        <div className='flex items-center gap-8'>
+        <div className='flex items-center gap-8 justify-between sm:justify-normal w-full sm:w-[80%]'>
             <Link className='text-primary-color font-[700] mr-5' href="/">
                 <Image src={logo} width="40" height="40" alt='Logo' />
             </Link>
-            <ul className='flex items-center gap-8'>
+            <ul className='hidden sm:flex items-center gap-8'>
                 {
                     navLinks.map((link, index) => (
                         <li key={index}>
@@ -43,11 +49,35 @@ export default function Navbar() {
                     ))
                 }
             </ul>
+
+            {/* Mobile Nav */}
+            {
+                mobileNavOpen &&
+                <div className='block sm:hidden fixed left-0 top-0 h-[100%] bg-black z-[999] text-white w-[45%]'>
+                    <div className='fixed top-0 left-0 w-[100%] h-full bg-[#000000ab] z-[1]'></div>
+                    <div onClick={() => setMobileNavOpen(false)} className='text-white text-right fixed right-[11px] top-[25px] z-[2] border text-[20px] p-1 cursor-pointer'>
+                        <IoCloseOutline className='cursor-pointer'/>
+                    </div>
+                    <ul className='fixed flex flex-col items-start pt-[6rem] w-[45%] h-full bg-black z-[999]'>
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index} className='w-full'>
+                                    <Link href={link.link} className='pl-[1rem] hover:bg-[#FF0200] transition-all w-full block py-4'>{link.label}</Link>
+                                </li>
+                            ))
+                        }
+                    <Link className='hover:bg-[#FF0200] transition-all ml-[1rem] mt-3 border py-[10px] px-7 text-white' href="/sign-in">Sign In</Link>
+                    </ul>
+                </div>
+            }
+            <div onClick={() => setMobileNavOpen(true)} className='text-gray-400 text-right border border-gray-400 text-[20px] block cursor-pointer sm:hidden bg-white p-1'>
+                <BiMenu className=''/>
+            </div>
         </div>
         <div className='flex items-center gap-5'>
             {/* <MdMail /> */}
             {/* <Link href="">Advertise</Link> */}
-            <Link className='bg-[#FF0200] rounded-[4px] px-[10px] py-[2px] text-white' href="/sign-in">Sign In</Link>
+            <Link className='bg-[#FF0200] rounded-[4px] px-[16px] py-[5px] text-white hidden sm:block' href="/sign-in">Sign In</Link>
         </div>
     </div>
   )
--- app/components/newly-posted-jobs-cards/NewlyPostedJobsCards.tsx
+++ app/components/newly-posted-jobs-cards/NewlyPostedJobsCards.tsx
@@ -18,8 +18,8 @@ export default function NewlyPostedJobsCards({ job }: NewlyPostedJobsCardProps) 
           alt={job.jobTitle}
           className="w-[50px] h-[50px] rounded-full mr-auto object-cover"
         />
-        <p className="mt-5 font-[500]">Position: <span className='font-[400]'>{job.jobTitle}</span> </p>
-        <p className="mt-1 font-[500]">Salary Range: <span className='font-[400]'>{job.salaryRange}</span> </p>
+        <p className="mt-5 font-[500] text-[12px] md:text-[14px] lg:text-[16px]">Position: <span className='font-[400]'>{job.jobTitle}</span> </p>
+        <p className="mt-1 font-[500] text-[12px] md:text-[14px] lg:text-[16px]">Salary Range: <span className='font-[400]'>{job.salaryRange}</span> </p>
       </div>
     </div>
   );
