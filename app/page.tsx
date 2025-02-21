60da61f770d892846a57163dbcfd24228c230df0
franklin-raph <igboekwulusifranklin@gmail.com>
2025-02-21 15:56:16 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,11 @@ import NewlyPostedJobsComponent from './components/newly-posted-jobs-component/N
 import BlogCard from './components/blog-card/BlogCard';
 import { IoIosArrowRoundForward } from 'react-icons/io';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { RxMinus, RxPlus } from 'react-icons/rx';
+import { PiPhoneCall } from 'react-icons/pi';
+import { IoChatboxEllipsesOutline, IoVideocamOutline } from 'react-icons/io5';
+import { FaRegMessage } from 'react-icons/fa6';
 
 // import { ChevronLeft, ChevronRight } from 'react-icons/lucide-react'
 
@@ -81,6 +86,50 @@ export default function Home() {
       date: "2023-2-12"
     }
   ]
+
+  const faqItems = [
+    {
+      question: 'What is Next.js?',
+      answer: 'Next.js is a React framework that enables functionality such as server-side rendering and generating static websites.',
+    },
+    {
+      question: 'What is Tailwind CSS?',
+      answer: 'Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.',
+    },
+    {
+      question: 'How do I use TypeScript with Next.js?',
+      answer: 'You can create a Next.js project with TypeScript by using the `--typescript` flag when setting up your project.',
+    },
+  ];
+
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const toggleAnswer = (index: number) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  const contactMethods = [
+    {
+      title: 'Call',
+      number: '08139362969',
+      icon: <PiPhoneCall />
+    },
+    {
+      title: 'Chat',
+      number: '08139362969',
+      icon: <IoChatboxEllipsesOutline />
+    },
+    {
+      title: 'Video Call',
+      number: '08139362969',
+      icon: <IoVideocamOutline />
+    },
+    {
+      title: 'Message',
+      number: '08139362969',
+      icon: <FaRegMessage />
+    }
+  ];
 
   const router = useRouter()
 
@@ -220,6 +269,7 @@ export default function Home() {
           </button>
         </div>
       </section>
+
       <section className='w-[100%] mx-auto mt-[6rem] bg-video flex flex-col items-center justify-center'>
         <div className="text-center flex flex-col items-center justify-center text-white mt-[60px]">
           <p className="font-[600] md:text-[30px] text-[20px]">
@@ -228,6 +278,78 @@ export default function Home() {
           <button onClick={() => router.push('/sign-up')} className="py-[10px] px-[20px] bg-[#FF0200] mt-[3rem] rounded-[8px]">Get Started</button>
         </div>
       </section>
+      
+      <section className="mt-[9rem] mb-[5rem]">
+        <div className="text-center mb-5">
+          <p className="text-[#212121] font-bold md:text-[30px] text-[20px]">Frequently Asked Questions</p>
+          <p className="text-[#777575] text-[14px] md:text-[16px]">Got Questions? We&apos;ve Got Answers!</p>
+        </div>
+        <div className="max-w-3xl mx-auto p-4">
+          {faqItems.map((item, index) => (
+            <div key={index} className="mb-4 border-b border-gray-200">
+              <button
+                onClick={() => toggleAnswer(index)}
+                className="w-full text-left py-2 flex justify-between items-center focus:outline-none"
+              >
+                <span className="text-md font-medium">{item.question}</span>
+                <span className="transform transition-transform duration-200">
+                  {activeIndex === index ? <RxMinus /> : <RxPlus />}
+                </span>
+              </button>
+              <div
+                className={`overflow-hidden transition-max-height duration-300 ease-in-out ${
+                  activeIndex === index ? 'max-h-40' : 'max-h-0'
+                }`}
+              >
+                <p className="py-2 text-[#212121] text-[14px] md:text-[16px]">{item.answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="md:max-w-[1600px] w-[95%] mx-auto lg:px-[2rem] flex items-start">
+        <div>
+          <div className="flex flex-col md:flex-row gap-12">
+            <div className="w-full md:w-1/2">
+              <h3 className="text-[#FF0200] font-medium mb-2">Contact Us</h3>
+              <h2 className="md:text-4xl text-xl font-bold text-navy-900 mb-4 text-[#212121]">Easy to contact us</h2>
+              <p className="text-[#212121] mb-8 text-[14px] md:text-[16px]">
+                We always ready to help by providing the best services for you. We beleive a good place to live can make your life better.
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-2">
+                {contactMethods.map((method, index) => (
+                  <div 
+                    key={index} 
+                    className="p-4 bg-white rounded-lg shadow-sm border contact cursor-pointer"
+                  >
+                    <div className="flex items-center gap-4 mb-4">
+                      <div className="w-[32px] h-[32px] bg-[#FF0200] rounded-full flex items-center justify-center text-white">
+                        {method.icon}
+                      </div>
+                      <div>
+                        <h3 className="font-medium md:text-lg">{method.title}</h3>
+                        <p className="text-[#121212] text-[14px] md:text-[16px]">{method.number}</p>
+                      </div>
+                    </div>
+                    <button className="w-full py-3 bg-[#ffeded] text-[#121212] rounded-lg hover:bg-red-100 transition-colors">
+                      Call Now
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img 
+                src="./images/contact.png"
+                alt="Contact Image"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
